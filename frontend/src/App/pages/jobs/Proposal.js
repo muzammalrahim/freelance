caff94560f23fa09e6e2e346b19395a4e2cded29
Muzammal Rahim <creative.joomdev@gmail.com>
2021-01-29 15:25:07 +0500

    newchnages

--- frontend/src/App/pages/jobs/Proposal.js
+++ frontend/src/App/pages/jobs/Proposal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './proposal.css';
 import './jobdetail.css';
+import Navbar from './Navbar';
 
 export default class Proposal extends Component {
     render() {
@@ -13,7 +14,22 @@ export default class Proposal extends Component {
                             <h3>Proposal Amount</h3>
                             <input type="text" placeholder="1"></input>
                         </li>
+                        <li>
+                            <h3>15% service free</h3>
+                            <input type="text" placeholder="1"></input>
+                        </li>
+                        <li>
+                            <h3>Total amount you’ll recieve</h3>
+                            <input type="text" placeholder="1"></input>
+                        </li>
+                        <li>
+                            <h3>Your Proposal</h3>
+                            <textarea row="3" placeholder="Text"></textarea>
+                        </li>
                     </ul>
+                    
+                    <button class="btn btn--yellow btn--medium">Submit a Proposal</button>
+                    
                 </div>
                 
             </div>
