43133a6fdc6259459f76f7cb257763808dd714dd
rajafaizan447 <70891380+rajafaizan447@users.noreply.github.com>
2021-02-02 02:54:46 -0500

    changes done in signin and signup pages

--- frontend/src/App/pages/signin/signuppage.js
+++ frontend/src/App/pages/signin/signuppage.js
@@ -1,16 +1,14 @@
 import React from 'react'
-
 import Button from '../../../components/Button';
 import InputField from '../../../components/InputField';
 import Linkedin from '../../../components/linkedin';
 import Navbar from '../../../components/profileview/Navbar';
-
 import './SignIn.css'
 import SiSoHero from '../../../components/SiSoHero';
 import loginimage from '../../../../src/loginimage.png';
 import AvatarImage from '../../../../src/AvatarImage.png';
-
-
+import LOCKER from '../../../assets/LOCKER.png'
+import eye from '../../../assets/eye.png'
 function SignIn() {
   return (
     <div className="SignUp-flex-container">
@@ -39,22 +37,27 @@ function SignIn() {
                   heading="Password"
                   Placeholder=" Enter Password"
                 />
-          
+               <img src={eye}  style={{position: 'relative', right: '25px'}} />
                 </div>
-               
-                <Button   buttonStyle="btn--Yellow" value="Forgot your password?"   />
+
+               <div className="Forgett">
+              
+              
+                 <Button buttonStyle="btn--Yellow" value="Forgot your password?"/> <span><img src={LOCKER} style={{position: 'relative', left: '349px', top: '359px'}}></img></span>
+                 
+               </div>
+                
               <div className="tos-check">
                 <input type="checkbox" id="tos" name="tos"></input>
                 <label for="tos"> </label>
                 <p>
-               Remember me</p>
-               
+               Remember me </p>
               </div>
               <div className="su-button-1">
                 <Button buttonStyle="btn--yellow1" value="Log In" />
               </div>
               <div className="su-button-2">
-                <p>Or</p>
+                <p style={{position: 'relative', top: '-16px'}}> Or</p>
               </div>
               <div className="su-button-3">
                 <Linkedin />
--- frontend/src/components/linkedin.js
+++ frontend/src/components/linkedin.js
@@ -3,7 +3,9 @@ import './linkedin.css';
 function linkedin() {
     return (
         <div>
-            <button className="btn-linkdin"><span className='title'> Sign up with Linked </span><i className="fab fa-linkedin"></i></button>
+            <button className="btn-linkdin">
+              <span className='title'> Sign up with <m>Linked</m> <i className="fab fa-linkedin"></i> </span>
+            </button>
         </div>
     )
 }
