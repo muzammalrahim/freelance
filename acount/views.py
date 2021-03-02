3489f7b8a23c06695cd640af050fa922d9b74fee
arslan <arslanmehmood051@gmail.com>
2021-03-02 18:31:36 +0500

    added country, Attachemen,image

--- acount/views.py
+++ acount/views.py
@@ -1,6 +1,6 @@
 from base64 import b64encode
 from rest_framework import viewsets
-#import face_recognition
+# import face_recognition
 
 from rest_framework.response import Response
 from allauth.socialaccount.providers.linkedin.views import LinkedInOAuthAdapter
@@ -20,6 +20,12 @@ class LinkedinLogin(SocialLoginView):
 class CityViewSet(viewsets.ModelViewSet):
     queryset = models.City.objects.all()
     serializer_class = acount_serializer.CitySerializers
+    search_fields = ['^name']
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = models.Country.objects.all()
+    serializer_class = acount_serializer.CountrySerializers
     search_fields = ['^name']
 
 
