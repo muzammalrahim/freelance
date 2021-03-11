3ec4ff0650129051fcaceab6bfd1372dac79f862
Taimoor Khan <taimoor36372@gmail.com>
2021-03-10 16:52:12 -0800

    working on linkedin

--- frontend/src/App/pages/signin/LinkedInPage.js
+++ frontend/src/App/pages/signin/LinkedInPage.js
@@ -10,26 +10,46 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 class LinkedInPage extends Component {
   state = {
-    code:"AQQumYDk85HEA4_zKcjWvxTvckrNwkzMIcJyHTirq3bmCaweHhHYOpaAGRenLvK9nzeCeZyK5SSlxjQPnQd-KsFcnQr3yeliB7gRIcP5rWCWl3ciAAuqk4YJSjwFVpgX8SevozRB_Cx9o3nzn2jUb6vpGspJjVzZ099XUwwdsieoe_RtIXjldXx0FKmol0g0c2TubvCjCTlFgGT3qr8",
+    code:"",
     errorMessage: "",
     access_token: "",
   };
 
   handleSuccess = (data) => {
-    let { code, access_token } = this.state;
-
+       console.log(' handleSuccess ', data );
     this.setState({
       code: data.code,
       errorMessage: "",
     });
 
-    if (data.code) {
-      GettingLinkedinAccessToken(data.code)
+  
+
+    if (this.state.code) {
+
+      console.log("if  this.state.code")
+
+       
+  
+
+      GettingLinkedinAccessToken(this.state.code)
         .then((response) => {
+      
+            console.log("response to access token :",response.data.access_token)
+              
           this.setState({ access_token: response.data.access_token });
 
-          if (response.data.access_token) {
-            Linkedinlogin(response.data.access_token, data.code)
+           console.log("a1 access token:",this.state.access_token)
+           console.log("a1 access code:",this.state.code)
+
+          if (this.state.access_token) {
+
+            const data = JSON.stringify({
+              access_token:this.state.access_token, 
+              code:this.state.code
+            })
+
+
+            Linkedinlogin(data)
               .then((response) => {
                 console.log("linkedin login response", response);
               })
@@ -52,6 +72,9 @@ class LinkedInPage extends Component {
     }
   };
 
+
+  
+
   handleFailure = (error) => {
     this.setState({
       code: "",
@@ -60,40 +83,52 @@ class LinkedInPage extends Component {
   };
 
   LinkedinLoginHandler = () => {
-    // Linkedinlogin()
-    // .then((response) => {
-    //
-    // console.log("linkedin reponse",response)
-    //
-    // })
-    // .catch(() => {
-    // disableLoading();
-    // });
+
   };
+
+
+  requestProfile = () => {
+    var oauthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&scope=r_liteprofile&state=123456&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`
+    var width = 450,
+      height = 730,
+      left = window.screen.width / 2 - width / 2,
+      top = window.screen.height / 2 - height / 2;
+
+    window.open(
+      oauthUrl,
+      "Linkedin",
+      "menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=" +
+        width +
+        ", height=" +
+        height +
+        ", top=" +
+        top +
+        ", left=" +
+        left
+    );
+  };
+
 
   render() {
     const { code, errorMessage, access_token } = this.state;
     return (
       <div>
-        <LinkedIn
-          clientId="86zeljscdjejfk"
-          onFailure={this.handleFailure}
-          onSuccess={this.handleSuccess}
-          redirectUri="http://localhost:3000/linkedin"
-        >
+       
           <button
             type="button"
             style={{ width: "39.5vw" }}
             className="btn btn-primary btn-block "
-            onClick={this.LinkedinLoginHandler}
+            onClick={this.requestProfile}
           >
             Signup with
             <span className="pl-2">
               Linked <LinkedInIcon />
             </span>
           </button>
-        </LinkedIn>
+  
    
+      <button onClick={this.tokenAccess}>  </button>
+
         {!code && <div>No code</div>}
         {code && <div>Code: {code}</div>}
         {code && <div>access token: {access_token}</div>}
