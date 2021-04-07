456114c56a9d649403ba0d6640ede1e6b9aaca51
arslan <arslanmehmood051@gmail.com>
2021-04-07 10:48:07 +0500

    work with user freelancer api

--- freelance/settings.py
+++ freelance/settings.py
@@ -170,6 +170,7 @@ REST_FRAMEWORK = {
 		'rest_framework.parsers.MultiPartParser',
 		'rest_framework.parsers.JSONParser',
 	],
+
 	'DEFAULT_FILTER_BACKENDS': [
 		'django_filters.rest_framework.DjangoFilterBackend',
 		'rest_framework.filters.SearchFilter',
